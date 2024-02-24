b3d09b383b2ba65fd890641a09668078aa7dca26
lumbermixalot <lumbermixalot@github.com>
2024-02-24 11:24:17 -0600

    Added exception catch when importing azlmbr modules that may not exist for some Tools like PassCanvas.
    Signed-off-by: lumbermixalot <lumbermixalot@github.com>

--- Gem/Editor/Scripts/bootstrap.py
+++ Gem/Editor/Scripts/bootstrap.py
@@ -5,16 +5,22 @@ SPDX-License-Identifier: Apache-2.0 OR MIT
 """
 # -------------------------------------------------------------------------
 
-
-import az_qt_helpers
-import azlmbr.editor as editor
-from camera_animator import CameraAnimatorDialog
+try:
+    import az_qt_helpers
+    import azlmbr.editor as editor
+    from camera_animator import CameraAnimatorDialog
+except Exception as err:
+    print(f"VolumetricClouds.boostrap Got an error error: {err}")
 
 if __name__ == "__main__":
     print("VolumetricClouds.boostrap")
 
     # Register our custom widget as a dockable tool with the Editor under an Examples sub-menu
-    options = editor.ViewPaneOptions()
-    options.showOnToolsToolbar = True
-    options.toolbarIcon = ":/${Name}/toolbar_icon.svg"
-    az_qt_helpers.register_view_pane('CameraAnimatorDialog', CameraAnimatorDialog, category="Examples", options=options)
+    try:
+        options = editor.ViewPaneOptions()
+        options.showOnToolsToolbar = True
+        options.toolbarIcon = ":/${Name}/toolbar_icon.svg"
+        az_qt_helpers.register_view_pane('CameraAnimatorDialog', CameraAnimatorDialog, category="Examples", options=options)
+    except:
+        print("VolumetricClouds.boostrap Failed to hook CameraAnimatorDialog in the Editor menu")
+
